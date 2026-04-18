d09c69d3454bdc1a280ed97a61e808ede943c208
Lochit Vinay <lochitvinay111@gmail.com>
2026-04-18 22:56:26 +0530

    all changes done

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 
+export const dynamic = "force-dynamic";
 
 const inter = Inter({ subsets: ["latin"] })
 
